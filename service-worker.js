73c930d478b74f2559cc55ddc0f167c409ab9dd2
Cataclas <caanclasan@gmail.com>
2024-11-26 14:35:27 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,25 @@
-// service-worker.js
 self.addEventListener('install', (event) => {
-    console.log('Service Worker instalado');
-    event.waitUntil(
-      caches.open('cache-v1').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/manifest.json',
-          '/static/js/main.js',
-          '/static/css/main.css',
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        return cachedResponse || fetch(event.request);
-      })
-    );
-  });
-  
+  console.log('Service Worker instalado');
+  event.waitUntil(
+    caches.open('cache-v1').then((cache) => {
+      return cache.addAll([
+        '/',                          // Página de inicio
+        '/index.html',                // HTML principal
+        '/manifest.json',             // Manifesto
+        '/static/js/main.js',         // JS compilado
+        '/static/css/main.css',       // CSS compilado
+      ])
+      .catch((error) => {
+        console.error('Error al almacenar en caché: ', error);
+      });
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
